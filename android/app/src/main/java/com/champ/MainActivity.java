0bbef91c36edda90218b5318e90bf9790b8f05af
MadhavNasit <40732694+MadhavNasit@users.noreply.github.com>
2020-09-28 10:04:26 +0530

    Android Splash Screen

--- android/app/src/main/java/com/champ/MainActivity.java
+++ android/app/src/main/java/com/champ/MainActivity.java
@@ -1,6 +1,7 @@
 package com.champ;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
         import com.facebook.react.ReactActivityDelegate;
         import com.facebook.react.ReactRootView;
@@ -8,12 +9,19 @@ import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
+  }
+
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
       @Override
       protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
       }
     };
   }
